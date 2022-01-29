e5268a2ca0a519f545abe9906e7c86a01525e20d
Hekari <sszpytma6@gmail.com>
2022-01-29 01:22:21 +0100

    testowy hamburger

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const hitem=document.querySelector(".fa-bars")
+const hamburger=document.querySelector(".burger")
+const ul=document.querySelector("ul")
+const nav=document.querySelector("nav")
+const hamburger_Visible=document.querySelector(".fa-bars")
+const hamburger_Close=document.querySelector(".fa-times")
+const burger = () => {
+    hamburger_Close.classList.toggle("hide")
+    hamburger_Visible.classList.toggle("hide")
+    ul.classList.toggle("active")
+
+}
+hamburger.addEventListener("click",burger)
